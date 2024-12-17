74d03cbde3173d1bde93864b05aaa52d05b10a19
Guilherme Carvalho <162490839+guicarbar@users.noreply.github.com>
2024-12-16 21:38:24 -0300

    final mobile-first (#18)
    - remove :root in create account.css
    - add css to gender and nasc
    - fixed somethings
    - create chang func - js

--- script/core.js
+++ script/core.js
@@ -1,3 +1,4 @@
+// options date nasc 
 function generatSelectionOptions(min, max, locate) {
   let select = document.getElementById(locate) 
   for (let i = min; i <= max; i++) {
@@ -6,12 +7,26 @@ function generatSelectionOptions(min, max, locate) {
     option.textContent = i
     select.appendChild(option)
   }
-
-  console.log("deu certo o do: " + locate)
 }
 
+// init 
 function initOptions() {
   generatSelectionOptions(1990, 2024, "year")
   generatSelectionOptions(1, 12, "mounth")
   generatSelectionOptions(1, 31, "day")
+}
+
+
+// change display btn func
+function change() {
+  let login = document.getElementById("loginArea")
+  let create = document.getElementById("createAccountArea")
+
+  if (login.style.display == "flex") {
+    login.style.display = "none"
+    create.style.display = "flex"
+  } else {
+    login.style.display = "flex"
+    create.style.display = "none"
+  }
 }
